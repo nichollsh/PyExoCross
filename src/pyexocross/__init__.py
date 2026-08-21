d64a70fbc6f1f6c925f5fde2c964f88b3780b74c
Harrison Nicholls <harrison.nicholls@ast.cam.ac.uk>
2026-08-21 15:40:50 +0200

    package version and authors

--- src/pyexocross/__init__.py
+++ src/pyexocross/__init__.py
@@ -34,7 +34,7 @@ Functions
 - ``px.download_exomol(...)``               -- Download ExoMol line-list files
 """
 
-__version__ = "1.1.13"
+__version__ = "1.2.0"
 
 from pyexocross.api import (
     # Run from .inp file
